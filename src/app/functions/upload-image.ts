c53e9c3adba0a3fbbe7e41a9d7a45e560f9c3254
Alexandre Moretti <alemoretti10@gmail.com>
2025-09-21 23:23:03 -0300

    Add file to upload image route

---
+++ src/app/functions/upload-image.ts
@@ -0,0 +1,10 @@
+import { Readable } from 'node:stream'
+import { z } from 'zod'
+
+const uploadImageInput = z.object({
+  fileName: z.string(),
+  contentType: z.string(),
+  contentStream: z.instanceof(Readable),
+})
+
+export async function uploadImage() {}

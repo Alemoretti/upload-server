3bda337c4ca12cf36821e0db07ac4de0aa77f522
Alexandre Moretti <alemoretti10@gmail.com>
2025-10-07 02:13:15 -0300

    Create getUploads endpoint

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -9,6 +9,7 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { getUploadsRoute } from './routes/get-uploads'
 import { uploadImageRoute } from './routes/upload-image'
 import { transformSwaggerSchema } from './transform-swagger-schema'
 
@@ -57,6 +58,7 @@ server.register(fastifySwaggerUi, {
 })
 
 server.register(uploadImageRoute)
+server.register(getUploadsRoute)
 
 server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`Server is running on http://localhost:${env.PORT}`)
